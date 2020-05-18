ae04fc767e5282f9e0ebd7cca938638559658366
Roman <roman@roman-pc.local>
2020-05-18 13:06:23 +0400

    Add code

---
+++ notebooks/ai/Занятие1/model.py
@@ -0,0 +1,37 @@
+from tensorflow.keras.datasets import mnist #Библиотека с базой Mnist
+from tensorflow.keras.models import Sequential # Подлючаем класс создания модели Sequential
+from tensorflow.keras.layers import Dense # Подключаем класс Dense - полносвязный слой
+from tensorflow.keras.optimizers import Adam # Подключаем оптимизатор Adam
+from tensorflow.keras import utils #Утилиты для to_categorical
+from tensorflow.keras.preprocessing import image #Для отрисовки изображения
+import numpy as np # Подключаем библиотеку numpy
+
+(x_train_org, y_train_org), (x_test_org, y_test_org) = mnist.load_data() #Загрузка данных Mnist
+
+#Меняем формат входных картинок с 28х28 на 784х1
+x_train = x_train_org.reshape(60000, 784)
+x_test = x_test_org.reshape(10000, 784)
+
+#Нормализуем входные картинки
+x_train = x_train.astype('float32') # преобразовываем x_train в тип float (цифры с плавающей точкой)
+x_train = x_train / 255 # делим на 255, чтобы диапазон был от 0 до 1
+x_test = x_test.astype('float32') # преобразовываем x_test в тип float (цифры с плавающей точкой)
+x_test = x_test / 255 # делим на 255, чтобы диапазон был от 0 до 1
+
+# Преобразуем ответы в формат one_hot_encoding
+y_train = utils.to_categorical(y_train_org, 10)
+y_test = utils.to_categorical(y_test_org, 10)
+
+
+model = Sequential() # Создаём сеть прямого распространения
+model.add(Dense(800, input_dim=784, activation="relu")) # Добавляем полносвязный слой на 800 нейронов с relu-активацией
+model.add(Dense(400, activation="relu")) # Добавляем полносвязный слой на 400 нейронов с relu-активацией
+model.add(Dense(10, activation="softmax")) # Добавляем полносвязный слой на 10 нейронов с softmax-активацией
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]) # Компилируем модель
+print(model.summary()) #Вывод структуры модели
+
+model.fit(x_train, y_train, batch_size=256, epochs=15, verbose=1)
+
+#print(model.predict(x_train[1015].reshape(1,784))) #Проверка натренированной модели на одном образце из тренировочного набора
+#print(y_train[1015])
